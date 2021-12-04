ea27c69413675c0f53e4b6e17d67e12461b5e9e7
Pablo G <pablobooks67@gmail.com>
2021-12-04 11:21:11 -0800

    all good

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-var captcha_version = 17;
 var captcha_done = false;
 var captcha_label = document.currentScript.getAttribute('label');
 var captcha_enemies = parseInt(document.currentScript.getAttribute('enemies'));
@@ -9,7 +8,7 @@ var captcha_html = '';
 if (captcha_label) {
     captcha_html = '<p>' + captcha_label + '<br>';
 }
-captcha_html += '<iframe id="doom_captcha" src="https://vivirenremoto.github.io/doomcaptcha/captcha.html?version=' + captcha_version + '&countdown=' + document.currentScript.getAttribute('countdown') + '&enemies=' + captcha_enemies + '" style="width:300px;height:150px;border:2px black solid;"></iframe>';
+captcha_html += '<iframe id="doom_captcha" src="https://vivirenremoto.github.io/doomcaptcha/captcha.html?countdown=' + document.currentScript.getAttribute('countdown') + '&enemies=' + captcha_enemies + '" style="width:300px;height:150px;border:2px black solid;"></iframe>';
 if (captcha_label) {
     captcha_html += '</p>';
 }
