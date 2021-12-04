a7905d7863ed2d095a6c5ddd700140dcdf3183be
Pablo G <pablobooks67@gmail.com>
2021-12-04 11:46:18 -0800

    AGH FIX

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ var captcha_html = '';
 if (captcha_label) {
     captcha_html = '<p>' + captcha_label + '<br>';
 }
-captcha_html += '<iframe id="doom_captcha" src="https://vivirenremoto.github.io/doomcaptcha/captcha.html?countdown=' + document.currentScript.getAttribute('countdown') + '&enemies=' + captcha_enemies + '" style="width:300px;height:150px;border:2px black solid;"></iframe>';
+captcha_html += '<iframe id="doom_captcha" src="https://its-pablo.github.io/99captcha/captcha.html?countdown=' + document.currentScript.getAttribute('countdown') + '&enemies=' + captcha_enemies + '" style="width:300px;height:150px;border:2px black solid;"></iframe>';
 if (captcha_label) {
     captcha_html += '</p>';
 }
