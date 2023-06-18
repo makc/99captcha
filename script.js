0445c1bb824897827feacce2f7b9f9dc2c3ca5ea
makc <makc.the.great@gmail.com>
2023-06-18 13:02:40 +0200

    un-fuck this up

--- script.js
+++ script.js
@@ -8,17 +8,11 @@ var captcha_html = '';
 if (captcha_label) {
     captcha_html = '<p>' + captcha_label + '<br>';
 }
-captcha_html += '<iframe id="doom_captcha" src="https://its-pablo.github.io/99captcha/captcha.html?countdown=' + document.currentScript.getAttribute('countdown') + '&enemies=' + captcha_enemies + '" style="width:300px;height:150px;border:2px black solid;"></iframe>';
+captcha_html += '<iframe id="doom_captcha" src="https://makc.github.io/99captcha/captcha.html?countdown=' + document.currentScript.getAttribute('countdown') + '&enemies=' + captcha_enemies + '" style="width:300px;height:150px;border:2px black solid;"></iframe>';
 if (captcha_label) {
     captcha_html += '</p>';
 }
 document.write(captcha_html);
-window.addEventListener('message', function(e) {
-    if (e.origin.indexOf('vivirenremoto.github.io') > -1) {
-        captcha_done = true;
-        document.getElementById('doom_captcha').style.borderColor = 'black';
-    }
-}, false);
 document.getElementById('doom_captcha').closest('form').addEventListener('submit', function() {
     if (!captcha_done) {
         document.getElementById('doom_captcha').style.borderColor = 'red';
